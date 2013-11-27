c4882a148a6c9af387f342497b9cce54f1eb09fd
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-11-28 00:48:17 +0100

    added --version flag

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 # -*- coding: latin-1 -*-
 from setuptools import setup
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
+    version = f.read()
 
 setup(name = 'abakaffe-cli',
-      version = '0.2',
+      version = version,
       description = 'A CLI for the Abakus Coffee API',
       author = 'Øyvind Robertsen, Martin Hallén',
       author_email = ['oyvindrobertsen@gmail.com', 'marthall@outlook.com'],
       url = 'http://github.com/oyvindrobertsen/abakaffe-cli',
-      py_modules = ['abakaffe'],
       install_requires = ['simplejson'],
       license = 'MIT',
       keywords = 'Abakus coffee API',
