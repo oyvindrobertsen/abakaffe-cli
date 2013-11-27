721c56def3c32fbcf4dbeff5a82361bc20ae40c0
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-11-27 15:09:50 +0100

    Added time differentiation

--- abakaffe.py
+++ abakaffe.py
@@ -2,6 +2,7 @@
 # -*- coding: latin-1 -*-
 import urllib2
 import simplejson
+from datetime import timedelta, datetime
 
 req = urllib2.Request("http://kaffe.abakus.no/api/status")
 opener = urllib2.build_opener()
@@ -10,8 +11,21 @@ json = simplejson.load(f)
 coffee = json['coffee']
 on = coffee['status']
 last_start = coffee['last_start']
+last_start = last_start.split(' ')
+last_start_date = last_start[0]
+last_start_time = last_start[1]
+last_start_date = last_start_date.split('-')
+last_start_time = last_start_time.split(':')
+dt = datetime(int(last_start_date[0]), int(last_start_date[1]), int(last_start_date[2]), int(last_start_time[0]), int(last_start_time[1]))
+
+delta = datetime.now() - dt
+
+hours, minutes, seconds = str(delta).split(':')
+
+
 
 if on:
-    print "Traktern er på"
+    print "Kaffetrakteren er på!"
 
-print "Kaffe ble sist traktet kl: " + last_start
+
+print "Kaffe ble sist traktet for " + hours + " timer siden, " + minutes + " minutter siden."
