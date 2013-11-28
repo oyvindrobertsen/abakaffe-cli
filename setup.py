c36b6e02a1756dd522b89cb9f3740bc4cd94ca55
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-11-28 01:40:15 +0100

    small fixes

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
     version = f.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
+    readme = f.read()
+
 setup(name = 'abakaffe-cli',
       version = version,
       description = 'A CLI for the Abakus Coffee API',
+      long_description = readme,
       author = 'Øyvind Robertsen, Martin Hallén',
       author_email = ['oyvindrobertsen@gmail.com', 'marthall@outlook.com'],
       url = 'http://github.com/oyvindrobertsen/abakaffe-cli',
