79bef533da8b01f815990efe61e4eb1b9c5bb47c
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-12-05 23:37:21 +0100

    Added a simple test

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,4 +1,11 @@
 from nose.tools import *
+from abakaffe.lib import Abakaffe
+from datetime import timedelta
 
-def test():
-    assert_equal(True, True)
+
+class test_status_suite():
+
+    def test_no_coffee_today(self):
+        time_delta = timedelta(1)
+        eq_("Det er ingen som har traktet kaffe i dag.",
+                    Abakaffe.get_status(time_delta))
